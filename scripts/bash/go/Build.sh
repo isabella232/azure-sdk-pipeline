715d00025cf273135f96720ac8995cbbf7e54863
FAREAST\chunyu <chunyu@microsoft.com>
2021-10-15 14:37:33 +0800

    use script in CodeGenerate.yml

--- scripts/bash/go/Build.sh
+++ scripts/bash/go/Build.sh
@@ -6,7 +6,8 @@ if [ -z $1 ]; then
 fi
 CODE_PATH=$1
 cd ${CODE_PATH}
-go get github.com/Azure/azure-sdk-for-go/sdk/azidentity@latest && go get github.com/Azure/azure-sdk-for-go/sdk/armcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/azcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/to@latest && go mod tidy
+# go get github.com/Azure/azure-sdk-for-go/sdk/azidentity@latest && go get github.com/Azure/azure-sdk-for-go/sdk/armcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/azcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/to@latest && go mod tidy
+go mod tidy
 # go mod download github.com/Azure/azure-sdk-for-go/sdk/armcore
 # go get github.com/Azure/azure-sdk-for-go/sdk/armcore@v0.8.0
 go build
