a8a16da020df282f47bd453fd50c1daa4e5836b8
Chun Yu <chunyu@microsoft.com>
2021-10-20 15:48:39 +0800

    update go mock test script

--- scripts/bash/go/RunMockTest.sh
+++ scripts/bash/go/RunMockTest.sh
@@ -11,8 +11,8 @@ echo $GOPATH
 echo $GOROOT
 
 go get gotest.tools/gotestsum
-ls -l $(Pipeline.Workspace)/s/go/bin
+ls -l $GOPATH/bin
 # go get github.com/Azure/azure-sdk-for-go/sdk/azidentity@latest && go get github.com/Azure/azure-sdk-for-go/sdk/armcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/azcore@latest && go get github.com/Azure/azure-sdk-for-go/sdk/to@latest && go mod tidy
 go mod tidy
-go get github.com/Azure/azure-sdk-for-go/sdk/$(ResourceProvider)/arm$(ResourceProvider)
-$GOROOT/gotestsum --format testname -- -coverprofile cover.out
+# go get github.com/Azure/azure-sdk-for-go/sdk/$(ResourceProvider)/arm$(ResourceProvider)
+$GOPATH/bin/gotestsum --format testname -- -coverprofile cover.out
