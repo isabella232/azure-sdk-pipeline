1987539e950593e6bb2ebcbb7e0b4fff4c4b5ccd
dw511214992 <dw511214992@gmail.com>
2021-11-25 17:05:44 +0800

    save

--- tools/sdk-automation/src/utils/SdkGenerationServerClient.ts
+++ tools/sdk-automation/src/utils/SdkGenerationServerClient.ts
@@ -1,32 +1,15 @@
 import {TaskResult} from "../lib/taskResult";
-import * as https from "https";
-import {logger} from "./logger";
+
 const axios = require('axios')
 
 export class SdkGenerationServerClient {
     host: string;
+
     constructor(host: string) {
         this.host = host;
     }
 
     public async publishTaskResult(sdkGenerationName: string, buildId: string, taskResult: TaskResult) {
-        // const request = https.request({
-        //         host: this.host,
-        //         path: `codegenerations/${sdkGenerationName}/taskResult`,
-        //         method: 'POST',
-        //         headers: {
-        //             'Accept': 'application/json',
-        //             'Content-Type': 'application/json; charset=UTF-8'
-        //         }
-        //     },
-        //     res => {
-        //         logger.info(res);
-        //     }
-        // )
-        // request.write(JSON.stringify(taskResult));
-        // request.on('error', (err) => {
-        //     throw err;
-        // });
         await axios.post(`https://${this.host}/codegenerations/${sdkGenerationName}/taskResult`, {
             pipelineBuildId: buildId,
             taskResult: taskResult
