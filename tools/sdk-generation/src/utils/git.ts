e991a46d1e2b30ef1fb934aafd473e3157142ce1
dw511214992 <dw511214992@gmail.com>
2021-11-26 21:48:13 +0800

    save

--- tools/sdk-generation/src/utils/git.ts
+++ tools/sdk-generation/src/utils/git.ts
@@ -2,6 +2,8 @@ import * as child_process from "child_process";
 
 
 export function getFileListInPackageFolder(packageFolder: string) {
-    const files = child_process.execSync('git ls-files -o',{ encoding: "utf8", cwd: packageFolder }).trim().split('\n');
+    child_process.execSync('git add .',{ encoding: "utf8", cwd: packageFolder });
+    const files = child_process.execSync('git ls-files',{ encoding: "utf8", cwd: packageFolder }).trim().split('\n');
+
     return files;
 }
