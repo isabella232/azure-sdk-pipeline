dd89706cfc47efad5e420529369cb4c34edfab55
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2022-03-28 13:26:34 +0800

    delete previous codes (#2232)

--- scripts/bash/dotnet/Build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $(Pipeline.Workspace)/s/$(SDK_REPO_NAME)/sdk/$(ResourceProvider)/Azure.ResourceManager.$(ResourceProvider)/src
-dotnet build
--- scripts/node/PublishResult.ts
+++
@@ -1,6 +0,0 @@
-const PublishResult = (confFile) => {
-    
-}
-
-const logfile =process.argv[2];
-PublishResult(logfile);
